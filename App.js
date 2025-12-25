81fd7299189df39f905dd68dfe804663f5e87d8c
muneeb063 <muneeb.asif063@gmail.com>
2025-12-26 01:43:46 +0500

    JSX

--- App.js
+++ App.js
@@ -1,31 +1,18 @@
 // {} this is used to give attributes to the class e.g, {id: "heading", }
 
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello, React from React.createElement!"
-);
+// rootReact.createElement => object => when rendere then it becomes HTMLElement
 
-console.log(heading); // object representation of the element
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 
-const root = createRoot(document.getElementById("root"));
+console.log(heading); // this will give object
+
+const jsxHeading = <h1 id = "heading">Hello World from JSX!</h1>
+
+console.log(jsxHeading); // this will also give object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
-
-// For nested elements
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child1" }, [
-    React.createElement("h1", {key: "child-1-h1"}, "I am H1"),
-    React.createElement("h2", {key: "child-1-h2"}, "I am H2"), // siblings should be in an array
-  ]),
-  React.createElement("div", { id: "child", key: "child2" }, [
-    React.createElement("h1", {key: "child-2-h1"}, "I am H1"),
-    React.createElement("h2", {key: "child-2-h2"}, "I am H2"), // siblings should be in an array
-  ]),
-]);
-console.log(parent);
-
-root.render(parent);
