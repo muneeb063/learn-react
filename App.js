a9f36893c86b36624f1d9de2cb53d6b69f01f981
muneeb063 <muneeb.asif063@gmail.com>
2025-12-31 14:54:02 +0500

    Restaurant Card using map

--- App.js
+++ App.js
@@ -28,27 +28,52 @@ const Header = () => {
 };
 
 const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { resName, cuisine, img, rating, time, price } = resData;
+
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-logo"
-        src="https://www.cafezouk.com/images/Special%20Dishes/Sp1.jpg"
-        alt="res-logo"
-      />
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>4.5 Stars</h4>
-      <h4>40 minutes</h4>
+      <img className="res-logo" src={img} alt="res-logo" />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{rating} Stars</h4>
+      <h4>{time}</h4>
+      <h4>{price}</h4>
     </div>
   );
 };
+
+const restaurantList = [
+  {
+    id: 1,
+    resName: "Burger King",
+    cuisine: "American",
+    img: "https://www.cafezouk.com/images/Special%20Dishes/Sp1.jpg",
+    rating: 4.5,
+    time: "40 minutes",
+    price: "₹300 for two",
+  },
+  {
+    id: 2,
+    resName: "Pizza Hut",
+    cuisine: "Italian",
+    img: "https://www.cafezouk.com/images/Special%20Dishes/Sp5.jpg",
+    rating: 4.5,
+    time: "40 minutes",
+    price: "₹400 for two",
+  },
+];
+
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard resName="Burger King" cuisine="American" />
-        <RestaurantCard resName="Pizza Hut" cuisine="Italian" />
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />
+        ))}
+        ;
       </div>
     </div>
   );
