854868d23f8bd9a7bcc24c5ba1a338372168b0d3
muneeb063 <muneeb.asif063@gmail.com>
2026-01-05 18:09:39 +0500

    Search / useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,67 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "../Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
+  const [filteredList, setFilteredList] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  let actualData = [];
 
   useEffect(() => {
-    console.log("Component Mounted UseEffect Function");
+    FetchData();
   }, []);
 
-  return (
+  const FetchData = async () => {
+    const data = await fetch(
+      "https://gist.githubusercontent.com/omar94hamza/c96f0be02bffa48056e12893be8eda36/raw/66e5f26a6013be9f1627310e676619ae4c87ce75/restaurants.json"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(json);
+    setFilteredList(json);
+  };
+
+  //COnditional Rendering
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredRestaurants = listOfRestaurants.filter(
+                (restaurant) =>
+                  restaurant.title
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase())
+              );
+              if (filteredRestaurants.length === 0) return;
+              setFilteredList(filteredRestaurants);
+              console.log(searchText);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -30,15 +80,16 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             console.log("Button Clicked");
-            setListOfRestaurants(restaurantList);
-            console.log(restaurantList);
+            //setListOfRestaurants(restaurantList);
+            FetchData();
+            // console.log(restaurantList);
           }}
         >
           All Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredList.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,16 +3,18 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const { resName, cuisine, img, rating, time, price } = resData;
+  // const { resName, cuisine, img, rating, time, price } = resData; //local Data
+  const { title, type, image, rating, titlMC, minCharge } = resData;
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img className="res-logo" src={CDN_URL + img} alt="res-logo" />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
+      {/* <img className="res-logo" src={CDN_URL + img} alt="res-logo" /> */}
+      <img className="res-logo" src={image} alt="res-logo" />
+      <h3>{title}</h3>
+      <h4>{type}</h4>
       <h4>{rating} Stars</h4>
-      <h4>{time}</h4>
-      <h4>{price}</h4>
+      <h4>{titlMC}</h4>
+      <h4>{minCharge}</h4>
     </div>
   );
 };
