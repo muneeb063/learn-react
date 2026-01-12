fd34035ca0549c7d50a980807239c32ef37f5c7a
muneeb063 <muneeb.asif063@gmail.com>
2026-01-12 19:21:15 +0500

    Class base model

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,35 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      <p>This is the About Us page of our application.</p>
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <p>This is the About Us page of our application.</p>
 
-      <UserClass name ={"Muhammad Muneeb (Class)"} location = {"Lahore, Pakistan"} />
-    </div>
-  );
-};
+        <UserClass
+          name={"Muhammad Muneeb (Class)"}
+          location={"Lahore, Pakistan"}
+        />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us Page</h1>
+//       <p>This is the About Us page of our application.</p>
+
+//       <UserClass
+//         name={"Muhammad Muneeb (Class)"}
+//         location={"Lahore, Pakistan"}
+//       />
+//     </div>
+//   );
+// };
 
 export default About;
